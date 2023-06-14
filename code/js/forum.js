c9320e068899cef4ffa6d888c6b24c8863a37464
ricotandrio <ricotandrio@gmail.com>
2023-06-14 11:29:26 +0700

    update_all

--- code/js/forum.js
+++ code/js/forum.js
@@ -1,15 +1,16 @@
-var forum_data = [
-    {"name": "User001", "date": "Wed Jun 14 2023 10:55:42 GMT+0700 (Western Indonesia Time)", "desc": "Hey, this is first forum message"},
-];
+// default value buat local storage
+var forum_data = [{"name": "User001", "date": "6/14/2023 11:16:34 AM", "desc": "Hey, this is first forum message"}];
 
+// buat ngecek 'forum_data' udh ada di localstorage ato kagak
 function initializeLocalStorageVariable(variableName, defaultValue) {
     if (!localStorage.getItem(variableName)) {
       localStorage.setItem(variableName, JSON.stringify(defaultValue));
     }
 }
-initializeLocalStorageVariable('forum_data', forum_data);
-const forum_data_storage = localStorage.getItem('forum_data');
 
+initializeLocalStorageVariable('forum_data', forum_data); // panggil function()
+
+const forum_data_storage = localStorage.getItem('forum_data');
 const container = document.getElementById("comment_section");
 function showComment(){
     var temp = "";
@@ -21,13 +22,21 @@ function showComment(){
                     <p>${temp_forum_data[i].desc}</p>
                 </div>`
     }
-
     container.innerHTML = temp;
 }
+
 showComment();
 
 function getname(){
     return "name";
+}
+
+function refresh(scrollX, scrollY){
+    // Refresh the page
+    location.reload();
+
+    // Restore the scroll position after the page is reloaded
+    window.scrollTo(scrollX, scrollY);
 }
 
 const formSend = document.getElementById("message_form");
@@ -39,11 +48,13 @@ formButton.addEventListener("click", function(event){
     var msg = document.getElementById("message_text");
     var currentDate = new Date();
     var timeEdit = (currentDate.toLocaleDateString() + " " + currentDate.toLocaleTimeString());
-    var forumDataArray = JSON.parse(forum_data_storage) || [];
-    
-    forumDataArray.push({"name": name, "date": timeEdit, "desc": msg.value});
 
-    localStorage.setItem('forum_data', JSON.stringify(forumDataArray));
+    var forumDataArray = JSON.parse(forum_data_storage); // ubah forum data array jdi array
+
+    forumDataArray.push({"name": name, "date": timeEdit, "desc": msg.value}); // push ke array
+
+    localStorage.setItem('forum_data', JSON.stringify(forumDataArray)); // masukin balik ke local storage
+    refresh(window.scrollX, window.scrollY);
     showComment();
     formSend.reset();
 });
