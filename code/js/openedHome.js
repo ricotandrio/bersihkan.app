6ae9326969dbe8367d42040a6890e710ba2189c6
ricotandrio <ricotandrio@gmail.com>
2023-06-14 23:12:22 +0700

    backup

--- code/js/openedHome.js
+++ code/js/openedHome.js
@@ -5,3 +5,15 @@ var userLogged = localStorage.getItem("userLogin");
 var loggedIn = JSON.parse(userLogged);
 
 temp.innerHTML = `<h1>Welcome, ${loggedIn.name}</h1>`
+
+document.getElementById("recycle").addEventListener("click", function(){
+    window.location.href = "../html/recycle.html";
+})
+
+document.getElementById("redeem").addEventListener("click", function(){
+    window.location.href = "../html/redeem.html";
+})
+
+document.getElementById("forum").addEventListener("click", function(){
+    window.location.href = "../html/forum.html";
+})
